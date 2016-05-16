e0af25fec67df17868a0972373f41a2d547854bb
Jenia Georgieva <jenia.95@abv.bg>
2016-05-16 12:08:23 +0300

    adding validation for user's symbol

--- tic_tac_toe_version2.sh
+++ tic_tac_toe_version2.sh
@@ -19,10 +19,41 @@ file=~/tic-tac-toe/$file_name
 touch $file
 
 #symbols for the two players
+touch symbol.tmp
+
 echo "\nThe symbol for $name1 is "
 read symbol1
+echo "$symbol1" >> symbol.tmp
+
+# validation of the first symbol
+while  [ $(( $(grep "[a-z0-9A-Z]" symbol.tmp| wc -m) - 1 )) -ne 1 ]
+do
+        echo "the symbol must be of one symbol [a-zA-Z0-9] "
+	read symbol1
+	echo "$symbol1" > symbol.tmp
+	if [ $(( $(grep "[a-z0-9A-Z]" symbol.tmp| wc -m) - 1 )) -eq 1 ]
+	then	
+		break
+	fi
+done
+
 echo "\nThe symbol for $name2 is "
 read symbol2
+echo "$symbol2" > symbol.tmp
+
+# validation of the second symbol
+while  [ $(( $(grep "[a-z0-9A-Z]" symbol.tmp| wc -m) -1 )) -ne 1 ]
+do
+        echo "the symbol must be of one symbol [a-zA-Z0-9] "
+        read symbol2
+        echo "$symbol2" > symbol.tmp
+	if [ $(( $(grep "[a-z0-9A-Z]" symbol.tmp| wc -m) )) -eq 1 ]
+	then
+		break
+	fi
+done
+
+rm symbol.tmp
 
 echo "\n$name1 plays with $symbol1" | tee -a $file
 echo "$name2 plays with $symbol2\n" | tee -a $file
@@ -58,7 +89,7 @@ win_test(){
 
 	if [ $win1 -eq 3 ]
 	then
-		echo "$name1, you win"
+		echo "$name1, you win" | tee -a $file
 		rm board.tmp
 		rm check.tmp
 		exit
@@ -67,7 +98,7 @@ win_test(){
 	win2=$(grep "$symbol2" check.tmp | wc -l)
 	if [ $win2 -eq 3 ]
 	then      
-		echo "$name2, you win"
+		echo "$name2, you win" | tee -a $file
 		rm board.tmp
 		rm check.tmp
 		exit
@@ -109,21 +140,21 @@ validation(){
                 read x1 y1
 		all_there $x1 $y1
         done
-	all_there $x1 $y1
 }
 
 # moves
 for moves in $(seq 1 9)
 do
-
 	# player one or player two
 	if [ $moves -eq 1 ] || [ $moves -eq 3 ] || [ $moves -eq 5 ] || [ $moves -eq 7 ] || [ $moves -eq 9 ]
 	then
-		echo "$name1, please enter a move [1-3] [1-3] " | tee -a $file
+		echo "$name1, please enter a move [1-3] [1-3] "
+		echo "$name1's " >> $file
 		read x1 y1
 		validation $x1 $y1
 	else
-		echo "$name2, please enter a move [1-3] [1-3] " | tee -a $file
+		echo "$name2, please enter a move [1-3] [1-3] "
+		echo "$name2's " >> $file
 		read x1 y1
 		validation $x1 $y1
 	
@@ -139,9 +170,7 @@ do
 		current_symbol=$symbol2
 	fi
 
-	# echo "move $moves symbol $current_symbol" | tee -a $file
-
-board $x1 $y1 $current_symbol
+	board $x1 $y1 $current_symbol
 
 	echo " *************" | tee -a $file
 	for i in $(seq 1 3)
@@ -160,7 +189,7 @@ board $x1 $y1 $current_symbol
 	done
 
 
-touch check.tmp
+	touch check.tmp
 
 # check for winning row	
 	for i in $(seq 1 3)
@@ -209,4 +238,4 @@ touch check.tmp
 done
 rm board.tmp
 rm check.tmp
-echo "Nobody wins"
+echo "Nobody wins" | tee -a $file
